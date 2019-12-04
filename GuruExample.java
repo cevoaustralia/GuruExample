20e9818be01f5632f037f27e21783a2c0cfaa142
Colin Panisset <colin.panisset@cevo.com.au>
2019-12-04 12:29:49 -0800

    more

--- GuruExample.java
+++ GuruExample.java
@@ -1,5 +1,9 @@
 /* An example of code that should get flagged by CodeGuru */
 import java.util.HashMap;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.IOException;
 
 class Whatsit {
     public int doit(int val) {
@@ -8,8 +12,8 @@ class Whatsit {
 }
 
 class GuruExample {
-    public static void main(String args[]) {
-        int len = Integer.parseInt(args[1]);
+    public static void main(String args[]) throws IOException {
+        int len = Integer.parseInt(args[0]);
         char[] thing = new char[len];
 
         for (int i=0; i<thing.length; i++) {
@@ -20,6 +24,8 @@ class GuruExample {
 
         HashMap<String, Whatsit> thang = new HashMap<String, Whatsit>();
         foo(thang);
+
+        spang();
     }
 
     public static synchronized void foo(HashMap<String, Whatsit> thing) {
@@ -28,6 +34,15 @@ class GuruExample {
             int result = whatsit.doit(123);
             thing.put("banana", whatsit);
         }
+    }
+
+    public static void spang() throws IOException {
+        Process p = Runtime.getRuntime().exec("date");
+        BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+        System.out.println("Got some spang: ");
+        System.out.println(in.readLine());
+        in.close();
     }
 }
 
