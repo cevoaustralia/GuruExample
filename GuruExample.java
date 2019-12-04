ddeddba866b8587a2bcb7398565f65fda9933ecc
Colin Panisset <colin.panisset@cevo.com.au>
2019-12-04 11:33:23 -0800

    Try and use before init to trigger review

--- GuruExample.java
+++ GuruExample.java
@@ -1,7 +1,12 @@
 /* An example of code that should get flagged by CodeGuru */
+import java.util.HashMap;
 
 class GuruExample {
     public static void main(String args[]) {
+        HashMap<String, String> foo;
+
+        foo.put("thing", "thing");
+
         int len = Integer.parseInt(args[1]);
         char[] thing = new char[len];
 
